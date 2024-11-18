75ea36c7ebc86de8f64add2d8d29c7f33c4617fa
MrDoda <kraldaniel.cze@gmail.com>
2024-11-18 17:02:57 +0100

    Update UserShoppingListManagement copy.tsx

--- ShoppingListFE/src/Components/UserShoppingListManagement copy.tsx
+++ ShoppingListFE/src/Components/UserShoppingListManagement copy.tsx
@@ -76,7 +76,7 @@ export const UserShoppingListManagement = ({
           </ListItem>
         ))}
       </List>
-      {isMember && (
+      {isOwner && (
         <>
           <Divider sx={{ marginY: 2 }} />
 
